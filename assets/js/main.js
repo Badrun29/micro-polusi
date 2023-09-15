ef75361bfa9b431ea783fb29b47ee16ea6aa173e
Badrun29 <spanma102@gmail.com>
2023-09-15 23:56:30 +0700

    edit2 titips

--- assets/js/main.js
+++ assets/js/main.js
@@ -303,3 +303,19 @@
    */
   new PureCounter();
 })();
+
+function updateTime() {
+  const currentTime = new Date();
+  const hours = currentTime.getHours().toString().padStart(2, "0");
+  const minutes = currentTime.getMinutes().toString().padStart(2, "0");
+  const seconds = currentTime.getSeconds().toString().padStart(2, "0");
+  const timeString = `${hours}:${minutes}:${seconds}`;
+
+  document.getElementById("current-time").textContent = timeString;
+}
+
+// Memanggil updateTime() pertama kali
+updateTime();
+
+// Memperbarui waktu setiap detik
+setInterval(updateTime, 1000);
